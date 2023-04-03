d36733b24cb6ce0813c7ee6e51ad8b02a419c56b
gabrielas98 <gabrielaszewczuk98@gmail.com>
2023-04-03 11:56:29 +0200

    First commit

---
+++ app/src/main/java/pl/pw/geoapp/MainActivity.kt
@@ -0,0 +1,64 @@
+package pl.pw.geoapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+import com.esri.arcgisruntime.ArcGISRuntimeEnvironment
+import com.esri.arcgisruntime.mapping.ArcGISMap
+import com.esri.arcgisruntime.mapping.BasemapStyle
+import com.esri.arcgisruntime.mapping.Viewpoint
+import com.esri.arcgisruntime.mapping.view.MapView
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var mapView: MapView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        mapView = findViewById<MapView>(R.id.mapView)
+
+        setApiKeyForApp()
+
+        setupMap()
+    }
+
+    override fun onPause() {
+        mapView.pause()
+        super.onPause()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        mapView.resume()
+    }
+
+    override fun onDestroy() {
+        mapView.dispose()
+        super.onDestroy()
+    }
+
+
+    // set up your map here. You will call this method from onCreate()
+    private fun setupMap() {
+
+        // create a map with the BasemapStyle streets
+        val map = ArcGISMap(BasemapStyle.ARCGIS_TOPOGRAPHIC)
+
+        // set the map to be displayed in the layout's MapView
+        mapView.map = map
+
+        // set the viewpoint, Viewpoint(latitude, longitude, scale)
+        mapView.setViewpoint(Viewpoint(34.0270, -118.8050, 72000.0))
+    }
+
+    private fun setApiKeyForApp(){
+        // set your API key
+        // Note: it is not best practice to store API keys in source code. The API key is referenced
+        // here for the convenience of this tutorial.
+
+        ArcGISRuntimeEnvironment.setLicense(R.string.arc_gis_license.toString())
+        ArcGISRuntimeEnvironment.setApiKey(R.string.maps_api_key.toString())
+
+    }
+}
